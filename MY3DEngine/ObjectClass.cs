e80a239e3181aea5bab52c4e77f1c170db124379
Michael Yahner <mjy8@zips.uakron.edu>
2013-06-16 20:44:40 -0400

    base class
    this will be the base class that will be serializable and used in the
    scene graph as the base

---
+++ MY3DEngine/ObjectClass.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace MY3DEngine
+{
+    [Serializable]
+    public class ObjectClass : IDisposable
+    {
+        public string Name { get; set; }
+        public string FileName { get; private set; }
+        public string FilePath { get; private set; }
+        [XmlIgnore]
+        public MeshClass MeshObject { get; protected set; }
+
+        #region Constructors
+
+        public ObjectClass()
+        {
+        }
+
+        public ObjectClass(string name = "", string fileName = "", string path = "")
+        {
+        }
+
+        #endregion
+
+        public virtual void Dispose()
+        {
+            MeshObject.Dispose();
+        }
+
+        public virtual void Renderer()
+        {
+            MeshObject.RenderMesh();
+        }
+    }
+}
