3cf99753854a4989cbcda6afbd19d4c79a83988e
senbeiwabaka <mjy8@zips.uakron.edu>
2013-07-13 16:53:35 -0400

    manages the list of objects that is added

--- MY3DEngine/ObjectManager.cs
+++ MY3DEngine/ObjectManager.cs
@@ -1,11 +1,68 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 
 namespace MY3DEngine
 {
-    class ObjectManager
+    public class ObjectManager
     {
+        private List<ObjectClass> _gameObjects = null;
+
+        public List<ObjectClass> GameObjects
+        {
+            get { return _gameObjects; }
+            set
+            {
+                if (_gameObjects == null)
+                {
+                    _gameObjects = new List<ObjectClass>();
+                }
+                _gameObjects = value;
+            }
+        }
+
+        public ObjectManager()
+        {
+            GameObjects = new List<ObjectClass>();
+        }
+
+        public bool AddObject(ObjectClass obj)
+        {
+            try
+            {
+                lock (GameObjects)
+                {
+                    GameObjects.Add(obj);
+                }
+            }
+            catch (Exception)
+            {
+                Engine.GameEngine.Exception.Exceptions.Add(new ExceptionData());
+                return false;
+            }
+
+            return true;
+        }
+
+        public bool RemoveObject()
+        {
+            try
+            {
+                lock (GameObjects)
+                {
+
+                }
+                Engine.GameEngine.Exception.Exceptions.Add(new ExceptionData("Not Implemented", "ObjectManager.RemoveObject", string.Empty));
+            }
+            catch (Exception e)
+            {
+                Engine.GameEngine.Exception.Exceptions.Add(new ExceptionData(e.Message, e.Source, e.StackTrace));
+                return false;
+            }
+
+            return true;
+        }
     }
 }
