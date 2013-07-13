6046855e032aaa97d92cbe9d4b05216b8f699103
senbeiwabaka <mjy8@zips.uakron.edu>
2013-07-13 16:54:02 -0400

    using objectmanager list to display objects

--- MY3DEngine/Renderer.cs
+++ MY3DEngine/Renderer.cs
@@ -10,11 +10,19 @@ namespace MY3DEngine
         {
             while (!Engine.GameEngine.IsNotShutDown)
             {
-                    Engine.GameEngine.LocalDevice.LocalDevice.Clear(ClearFlags.Target | ClearFlags.ZBuffer, Color.Black, 1.0f, 0);
-                    Engine.GameEngine.LocalDevice.LocalDevice.BeginScene();
+                Engine.GameEngine.LocalDevice.ThisDevice.Clear(ClearFlags.Target | ClearFlags.ZBuffer, Color.Black, 1.0f, 0);
+                Engine.GameEngine.LocalDevice.ThisDevice.BeginScene();
 
-                    Engine.GameEngine.LocalDevice.LocalDevice.EndScene();
-                    Engine.GameEngine.LocalDevice.LocalDevice.Present();
+                lock (Engine.GameEngine.Manager.GameObjects)
+                {
+                    foreach (var item in Engine.GameEngine.Manager.GameObjects)
+                    {
+                        item.Renderer();
+                    }
+                }
+
+                Engine.GameEngine.LocalDevice.ThisDevice.EndScene();
+                Engine.GameEngine.LocalDevice.ThisDevice.Present();
             }
         }
     }
