bea7c86767d5cdb797aef6c055613cc6fae76344
Michael Yahner <MichaelYahner@outlook.com>
2017-08-26 18:01:52 -0400

    triangles with textures - initial

---
+++ MY3DEngine/Common/TextureLoader.cs
@@ -0,0 +1,65 @@
+﻿using SharpDX.Direct3D11;
+using SharpDX.WIC;
+
+namespace MY3DEngine.Common
+{
+    public static class TextureLoader
+    {
+        /// <summary>
+        /// Loads a bitmap using WIC.
+        /// </summary>
+        /// <param name="factory"></param>
+        /// <param name="filename"></param>
+        /// <returns></returns>
+        public static BitmapSource LoadBitmap(ImagingFactory2 factory, string filename)
+        {
+            var bitmapDecoder = new BitmapDecoder(
+                factory,
+                filename,
+                DecodeOptions.CacheOnDemand
+                );
+
+            var formatConverter = new FormatConverter(factory);
+
+            formatConverter.Initialize(
+                bitmapDecoder.GetFrame(0),
+                PixelFormat.Format32bppPRGBA,
+                BitmapDitherType.None,
+                null,
+                0.0,
+                BitmapPaletteType.Custom);
+
+            return formatConverter;
+        }
+
+        /// <summary>
+        /// Creates a <see cref="Texture2D"/> from a WIC <see cref="BitmapSource"/>
+        /// </summary>
+        /// <param name="device">The Direct3D11 device</param>
+        /// <param name="bitmapSource">The WIC bitmap source</param>
+        /// <returns>A Texture2D</returns>
+        public static Texture2D CreateTexture2DFromBitmap(Device device, BitmapSource bitmapSource)
+        {
+            // Allocate DataStream to receive the WIC image pixels
+            int stride = bitmapSource.Size.Width * 4;
+            using (var buffer = new SharpDX.DataStream(bitmapSource.Size.Height * stride, true, true))
+            {
+                // Copy the content of the WIC to the buffer
+                bitmapSource.CopyPixels(stride, buffer);
+                return new Texture2D(device, new Texture2DDescription()
+                {
+                    Width = bitmapSource.Size.Width,
+                    Height = bitmapSource.Size.Height,
+                    ArraySize = 1,
+                    BindFlags = BindFlags.ShaderResource,
+                    Usage = ResourceUsage.Immutable,
+                    CpuAccessFlags = CpuAccessFlags.None,
+                    Format = SharpDX.DXGI.Format.R8G8B8A8_UNorm,
+                    MipLevels = 1,
+                    OptionFlags = ResourceOptionFlags.None,
+                    SampleDescription = new SharpDX.DXGI.SampleDescription(1, 0),
+                }, new SharpDX.DataRectangle(buffer.DataPointer, stride));
+            }
+        }
+    }
+}
--- MY3DEngine/Primitives/TriangleWithTexture.cs
+++ MY3DEngine/Primitives/TriangleWithTexture.cs
@@ -1,17 +1,134 @@
 ﻿using MY3DEngine.BaseObjects;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using MY3DEngine.Common;
+using SharpDX;
+using SharpDX.D3DCompiler;
+using SharpDX.Direct3D11;
+using SharpDX.DXGI;
+using System.IO;
+using wic = SharpDX.WIC;
 
 namespace MY3DEngine.Primitives
 {
+    internal struct VertexPos
+    {
+        public Vector3 pos;
+        public Vector2 tex0;
+
+        public VertexPos(Vector3 pos, Vector2 tex0)
+        {
+            this.pos = pos;
+            this.tex0 = tex0;
+        }
+    };
+    
     public class TriangleWithTexture : GameObjectWithTexture
     {
         public TriangleWithTexture()
         {
             this.Name = "Triangle with Texture";
+        }
+
+        public override void LoadContent()
+        {
+            var path = Path.GetFullPath("Shaders");
+
+            // Compile Vertex shaders
+            using (var vertexShaderByteCode = ShaderBytecode.CompileFromFile(string.Format("{0}\\TriangleWithTexture.fx", path), "VS_Main", "vs_4_0", ShaderFlags.EnableStrictness | ShaderFlags.Debug, EffectFlags.None))
+            {
+                this.VertextShader = new VertexShader(Engine.GameEngine.GraphicsManager.GetDevice, vertexShaderByteCode);
+
+                this.inputLayout = new InputLayout(
+                    Engine.GameEngine.GraphicsManager.GetDevice,
+                    ShaderSignature.GetInputSignature(vertexShaderByteCode),
+                    new[]
+                    {
+                        new InputElement("POSITION", 0, Format.R32G32B32A32_Float, 0, 0, InputClassification.PerVertexData, 0),
+                        new InputElement("TEXCOORD", 0, Format.R32G32_Float, 12, 0, InputClassification.PerVertexData, 0)
+                    });
+            }
+
+            // Compile Pixel shaders
+            using (var pixelShaderByteCode = ShaderBytecode.CompileFromFile(string.Format("{0}\\TriangleWithTexture.fx", path), "PS_Main", "ps_4_0", ShaderFlags.EnableStrictness | ShaderFlags.Debug, EffectFlags.None))
+            {
+                this.PixelShader = new PixelShader(Engine.GameEngine.GraphicsManager.GetDevice, pixelShaderByteCode);
+            }
+
+            // Instantiate Vertex buffer from vertex data
+            this.buffer = Buffer.Create(
+                Engine.GameEngine.GraphicsManager.GetDevice,
+                BindFlags.VertexBuffer,
+                new[]
+                {
+                        new VertexPos(new Vector3(0.5f, 0.5f, 0.5f), new Vector2(1.0f, 1.0f)),
+                        new VertexPos(new Vector3(0.5f, -0.5f, 0.5f), new Vector2(1.0f, 0.0f)),
+                        new VertexPos(new Vector3(-0.5f, -0.5f, 0.5f), new Vector2(0.0f, 0.0f)),
+
+                        new VertexPos(new Vector3(-0.5f, -0.5f, 0.5f), new Vector2(0.0f, 0.0f)),
+                        new VertexPos(new Vector3(-0.5f, 0.5f, 0.5f), new Vector2(0.0f, 1.0f)),
+                        new VertexPos(new Vector3(0.5f, 0.5f, 0.5f), new Vector2(1.0f, 1.0f))
+                });
+
+            path = Path.GetFullPath("Assets");
+            path = string.Format("{0}\\decal.png", path);
+
+            // initialize the WIC factory
+            using (var imagingFactory = new wic.ImagingFactory2())
+            {
+                //using (var decoder = new SharpDX.WIC.PngBitmapDecoder(imagingFactory))
+                //{
+                //    decoder.Initialize(new wic.WICStream(imagingFactory, path, NativeFileAccess.All), wic.DecodeOptions.CacheOnDemand);
+
+                //    using (var formatConverter = new SharpDX.WIC.FormatConverter(imagingFactory))
+                //    {
+                //        formatConverter.Initialize(decoder.GetFrame(0), SharpDX.WIC.PixelFormat.Format32bppPRGBA, SharpDX.WIC.BitmapDitherType.None, null, 0.0, SharpDX.WIC.BitmapPaletteType.Custom);
+
+                //        SharpDX.DataStream dataStream = new SharpDX.DataStream(formatConverter.Size.Height * formatConverter.Size.Width * 4, true, true);
+                //        formatConverter.CopyPixels(formatConverter.Size.Width * 4, dataStream);
+
+                //        //_icons.Add(iconInfo.Name, new SharpDX.Direct2D1.Bitmap(renderTarget, new SharpDX.Size2(formatConverter.Size.Width, formatConverter.Size.Height), dataStream,
+                //        //    formatConverter.Size.Width * 4, bitmapProperties));
+                //    }
+                //}
+
+
+                //var inputStream = new wic.WICStream(imagingFactory, path, NativeFileAccess.All); // open the image file for reading
+                // var decoder = new wic.BitmapDecoder(imagingFactory, inputStream, wic.DecodeOptions.CacheOnDemand);
+
+                //var t =  decoder.QueryInterface<Texture2D>();
+
+
+                using (var texture = TextureLoader.CreateTexture2DFromBitmap(Engine.GameEngine.GraphicsManager.GetDevice, TextureLoader.LoadBitmap(new wic.ImagingFactory2(), path)))
+                {
+                    this.ColorMap = new ShaderResourceView(Engine.GameEngine.GraphicsManager.GetDevice, texture);
+                }
+            }
+
+            var samplerStateDescription = new SamplerStateDescription()
+            {
+                AddressU = TextureAddressMode.Wrap,
+                AddressV = TextureAddressMode.Wrap,
+                AddressW = TextureAddressMode.Wrap,
+                ComparisonFunction = Comparison.Never,
+                Filter = Filter.MinMagMipLinear,
+                MaximumLod = float.MaxValue
+            };
+
+            this.ColorMapSampler = new SamplerState(Engine.GameEngine.GraphicsManager.GetDevice, samplerStateDescription);
+        }
+
+        public override void Render()
+        {
+            Engine.GameEngine.GraphicsManager.GetDeviceContext.InputAssembler.InputLayout = this.inputLayout;
+            Engine.GameEngine.GraphicsManager.GetDeviceContext.InputAssembler.PrimitiveTopology = SharpDX.Direct3D.PrimitiveTopology.TriangleList;
+            Engine.GameEngine.GraphicsManager.GetDeviceContext.InputAssembler.SetVertexBuffers(0, new VertexBufferBinding(this.buffer, 32, 0));
+
+            Engine.GameEngine.GraphicsManager.GetDeviceContext.VertexShader.Set(this.VertextShader);
+
+            Engine.GameEngine.GraphicsManager.GetDeviceContext.PixelShader.Set(this.PixelShader);
+            Engine.GameEngine.GraphicsManager.GetDeviceContext.PixelShader.SetShaderResource(0, this.ColorMap);
+            Engine.GameEngine.GraphicsManager.GetDeviceContext.PixelShader.SetSampler(0, this.ColorMapSampler);
+
+            Engine.GameEngine.GraphicsManager.GetDeviceContext.Draw(6, 0);
         }
     }
 }
