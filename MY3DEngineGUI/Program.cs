4f91bad814110077bdf32337037d6f0da0dc8c3c
Michael Yahner <michaelyahner@outlook.com>
2017-11-25 08:43:49 -0500

    added exception catching

--- MY3DEngineGUI/Program.cs
+++ MY3DEngineGUI/Program.cs
@@ -11,9 +11,32 @@ namespace MY3DEngineGUI
         [STAThread]
         static void Main()
         {
+            // Set the unhandled exception mode to force all Windows Forms errors
+            // to go through our handler.
+            Application.SetUnhandledExceptionMode(UnhandledExceptionMode.CatchException);
+            
+            AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
+
+            // Add the event handler for handling UI thread exceptions to the event.
+            Application.ThreadException += Application_ThreadException;
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+        }
+
+        private static void Application_ThreadException(object sender, System.Threading.ThreadExceptionEventArgs e)
+        {
+            var exception = e.Exception;
+
+            MY3DEngine.Engine.GameEngine.Exception.Exceptions.Add(new MY3DEngine.ExceptionData(exception?.Message, exception?.Source, exception?.StackTrace));
+        }
+
+        private static void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            var exception = e.ExceptionObject as Exception;
+
+            MY3DEngine.Engine.GameEngine.Exception.Exceptions.Add(new MY3DEngine.ExceptionData(exception?.Message, exception?.Source, exception?.StackTrace));
         }
     }
 }
